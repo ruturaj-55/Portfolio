ba298672a0b56df64d0b582b14d2acc4b6548869
Ruturaj Patil <ruturajp551@gmail.com>
2022-08-22 16:29:36 +0530

    Firebase deployed

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB66snysokaWnO2FmNNk_kSkFnKEMqRMb0",
-  authDomain: "portfolio-elessar.firebaseapp.com",
-  projectId: "portfolio-elessar",
-  storageBucket: "portfolio-elessar.appspot.com",
-  messagingSenderId: "458936332963",
-  appId: "1:458936332963:web:760571a1592d641e268516",
-  measurementId: "G-4K3PT94208",
+  apiKey: "AIzaSyCJk7CmKdFE3rsIfBeBsEqIiHWMr83Z6Y8",
+  authDomain: "elessar-portfolio.firebaseapp.com",
+  projectId: "elessar-portfolio",
+  storageBucket: "elessar-portfolio.appspot.com",
+  messagingSenderId: "824367513657",
+  appId: "1:824367513657:web:6d24c0c4b2f7400dda8de8",
+  measurementId: "G-87BSL8H3J0",
 };
 
 // Initialize Firebase
